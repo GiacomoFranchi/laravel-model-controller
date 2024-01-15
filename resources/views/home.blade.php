7263b83976e118cf5a7726644a0de040ccf9f2a0
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-01-15 17:30:39 +0100

    add: movies-card and style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>HELL COME TO LARAVEL</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>
-                {{$movie->title}}
-            </li>
-        @endforeach
-    </ul>
+    <h1 class="text-center text-dark fs-1 fw-bolder my-3">MyFLIX</h1>
+    <div class="container">
+        <div class="row row-cols-5">
+            @foreach ($movies as $movie)
+                <div class="col my-3 d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="{{$movie->image}}" class="card-img-top" alt="{{$movie->title}} poster">
+                        <div class="descr">
+                            <span>Title:</span>
+                            <h2>{{$movie->title}}</h2>
+                            <span>Original-Title:</span>
+                            <h3>{{$movie->original_title}}</h3>
+                            <div class="info">
+                                <span >{{$movie->vote}}<i class="fa-solid fa-star"></i></span>
+                                <span>{{$movie->date}} <i class="fa-regular fa-calendar-days"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
